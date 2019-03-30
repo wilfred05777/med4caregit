18c0bf78921562ababe5d48702523a67070fbd68
Wilfred Erdo <wilfred05777@gmail.com>
2019-03-30 10:04:40 +0800

    update front page

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -11,6 +11,7 @@
                 </div>
                 <div class="card-body">
                     @lang('strings.frontend.welcome_to', ['place' => app_name()])
+
                 </div>
             </div><!--card-->
         </div><!--col-->
@@ -18,23 +19,23 @@
 
     <div class="row mb-4">
         <div class="col">
-            <example-component></example-component>
+            {{--<example-component></example-component>--}}
         </div><!--col-->
     </div><!--row-->
 
-    <div class="row">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">
-                    <i class="fab fa-font-awesome-flag"></i> Font Awesome @lang('strings.frontend.test')
-                </div>
-                <div class="card-body">
-                    <i class="fas fa-home"></i>
-                    <i class="fab fa-facebook"></i>
-                    <i class="fab fa-twitter"></i>
-                    <i class="fab fa-pinterest"></i>
-                </div><!--card-body-->
-            </div><!--card-->
-        </div><!--col-->
-    </div><!--row-->
+    {{--<div class="row">--}}
+        {{--<div class="col">--}}
+            {{--<div class="card">--}}
+                {{--<div class="card-header">--}}
+                    {{--<i class="fab fa-font-awesome-flag"></i> Font Awesome @lang('strings.frontend.test')--}}
+                {{--</div>--}}
+                {{--<div class="card-body">--}}
+                    {{--<i class="fas fa-home"></i>--}}
+                    {{--<i class="fab fa-facebook"></i>--}}
+                    {{--<i class="fab fa-twitter"></i>--}}
+                    {{--<i class="fab fa-pinterest"></i>--}}
+                {{--</div><!--card-body-->--}}
+            {{--</div><!--card-->--}}
+        {{--</div><!--col-->--}}
+    {{--</div><!--row-->--}}
 @endsection
